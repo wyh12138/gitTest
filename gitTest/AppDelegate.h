166f075f31e8c92c5b1bcee3cc652be9ba24c04c
xxg415 <xxg415@xxg415deMac-Pro.local>
2019-03-26 09:53:15 +0800

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by xxg415 on 2019/3/26.
+//  Copyright © 2019 xxg415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by xxg415 on 2019/3/26.
+//  Copyright © 2019 xxg415. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
